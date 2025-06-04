2b75f3ee338ab7c84d88202b3ea3c9c0060a8a1d
Prerana Hippargi <136839194+preranah7@users.noreply.github.com>
2025-06-04 18:52:06 +0530

    Doubly LinkedList methods

---
+++ LinkedLists/DoubleLL.java
@@ -0,0 +1,148 @@
+package LinkedListLec;
+
+public class DoubleLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size; 
+
+    //print
+    public void print(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+"<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    //add
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        newNode.next = null;
+        newNode = tail;
+
+    }
+
+    public void add(int idx,int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i=0; //track temp idx
+        while(i<idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next.prev = newNode;
+        newNode.prev = temp;
+        temp.next = newNode;
+
+    }
+
+
+    //remove
+    public int removeFirst(){
+        if(head==null){
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+
+    public int removeLast(){
+        if(head==null){
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+
+    public void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(5);
+        dll.addFirst(4);
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+        dll.add(3, 9);
+        dll.print();
+    }
+}
