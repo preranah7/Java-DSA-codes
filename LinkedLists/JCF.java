efa1147037454a37c877547f8fa9b840c947587e
Prerana Hippargi <136839194+preranah7@users.noreply.github.com>
2025-06-04 18:52:46 +0530

    LL using Java Collection Framework

---
+++ LinkedLists/JCF.java
@@ -0,0 +1,21 @@
+package LinkedListLec;
+import java.util.LinkedList; //JCF
+public class JCF {
+    public static void main(String[] args) {
+        //create
+        LinkedList<Integer>ll = new LinkedList<>();
+
+        //add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        //0->1->2
+        System.out.println(ll);
+
+        //remove
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+
+    }
+}
