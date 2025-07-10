1390eead94f37551fadc8a65b7db69176b7686a7
Prerana Hippargi <136839194+preranah7@users.noreply.github.com>
2025-07-10 11:58:55 +0530

    pair sum- brute and two pointer approach

---
+++ ArrayList/PairSum1.java
@@ -0,0 +1,52 @@
+package Arraylist;
+import java.util.*;
+public class PairSum1 {
+
+    public static boolean pairSum1(ArrayList<Integer>list, int target){//brute force
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                if(list.get(i) + list.get(j) == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    //2pointer approach
+    public static boolean pairSumII(ArrayList<Integer>list, int target){
+        int lp = 0;
+        int rp = list.size() - 1;
+        while(lp!=rp){
+            //case 1
+            if(list.get(lp) + list.get(rp) == target){
+                return true;
+            }
+
+            //case 2
+            if(list.get(lp) + list.get(rp) < target){
+                lp++;
+            }else{
+                //case 3
+                rp--;
+            }
+        }
+        return false;
+    }
+
+    
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        //1, 2, 3, 4, 5, 6
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 50;
+
+        System.out.println(pairSumII(list, target));
+    }
+}
